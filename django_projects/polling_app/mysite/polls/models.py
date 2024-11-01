0d03214494c86b76c7f4f93605e7850924e97543
Alloh Nkefor Rodrick <allohnkeforrodrick@gmail.com>
2024-11-01 13:41:55 +0100

    Editing and modifying the admin page to give more functionality and control to our structured app

--- django_projects/polling_app/mysite/polls/models.py
+++ django_projects/polling_app/mysite/polls/models.py
@@ -1,6 +1,7 @@
 import datetime
 from django.utils import timezone
 from django.db import models
+from django.contrib import admin
 
 # Create your models here.
 class Question(models.Model):
@@ -21,4 +22,13 @@ class Choice(models.Model):
     
     def __str__(self):
         return self.choice_text
+    
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
     
